ac85de011923d61e25df979faf5f9914ae51f98d
JewelReed <jewelreed@myyahoo.com>
2026-02-14 15:14:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 300;
 
 /* ================================
    Express Setup
